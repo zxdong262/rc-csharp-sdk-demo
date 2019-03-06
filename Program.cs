1c1117f7f4cacfd29d12920416405dcad6bdfcce
ZHAO Xudong <zxdong@gmail.com>
2019-03-06 13:31:10 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using dotenv.net;
+using RingCentral;
+
+namespace rc_csharp_sdk_demo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          DotEnv.Config();
+          Console.WriteLine("Hello World!");
+          MainAsync().Wait();
+        }
+        static async Task MainAsync() {
+          using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var result = await rc.Restapi().Account().Get();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rc-csharp-sdk-demo
+
 Demo app with RingCentral C# SDK
+
+## Run
+
+```bash
+git clone git@github.com:zxdong262/rc-csharp-sdk-demo.git
+cd rc-csharp-sdk-demo
+dotnet run
+
+# will show
+Hello World!
+RingCentral.GetAccountInfoResponse
+```

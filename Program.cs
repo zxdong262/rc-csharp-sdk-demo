58daf90af7248aebfc5aa134b5cb2b545aaed35e
ZHAO Xudong <zxdong@gmail.com>
2019-03-06 13:46:17 +0800

    Format code

--- Program.cs
+++ Program.cs
@@ -6,29 +6,30 @@ using RingCentral;
 namespace rc_csharp_sdk_demo
 {
 
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          DotEnv.Config();
-          Console.WriteLine("Hello World!");
-          MainAsync().Wait();
-        }
-        static async Task MainAsync() {
-          using (var rc = new RestClient(
-                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
-                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
-                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
-            ))
-            {
-                await rc.Authorize(
-                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
-                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
-                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
-                );
-                var result = await rc.Restapi().Account().Get();
-                Console.WriteLine(result);
-            }
-        }
+      DotEnv.Config();
+      Console.WriteLine("Hello World!");
+      MainAsync().Wait();
     }
+    static async Task MainAsync()
+    {
+      using (var rc = new RestClient(
+          Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+          Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+          Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+      ))
+      {
+        await rc.Authorize(
+            Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+            Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+            Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+        );
+        var result = await rc.Restapi().Account().Get();
+        Console.WriteLine(result);
+      }
+    }
+  }
 }
